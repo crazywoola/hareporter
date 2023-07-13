9bbdbf05ac0afe5f6432ddd06e469a4ca1f6e517
crazywoola <427733928@qq.com>
2023-07-13 11:22:07 +0800

    init

---
+++ README.md
@@ -0,0 +1,21 @@
+# HaReporter
+
+哈利波特乔装成了一个记者，准备偷袭伏地魔。 哈利波特：“你好我是一名记者。” 伏地魔：“ha？reporter?” 哈利波特：“还是被你发现了！”
+
+## Quickstart
+
+```
+npm install
+npm start
+```
+
+## Docker
+
+```
+docker run -d IMAGE_ID
+```
+
+## Links
+[discord.js doc](https://old.discordjs.dev/#/docs/discord.js/main/general/welcome)  
+[discord dev portal](https://discord.com/developers/)  
+[Replit](https://replit.com/)  
---
+++ index.js
@@ -0,0 +1,54 @@
+import dotenv from "dotenv";
+import { Client, Events, GatewayIntentBits } from "discord.js";
+
+dotenv.config();
+import { ChatClient } from "dify-client";
+
+// Create a new client instance
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers,
+  ],
+});
+
+// When the client is ready, run this code (only once)
+// We use 'c' for the event parameter to keep it separate from the already defined 'client'
+client.once(Events.ClientReady, (c) => {
+  console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+client.on(Events.MessageCreate, async (message) => {
+  // console.log(message.channel)
+  const inputs = {};
+  const user = `${message.author.username}-${message.author.id}`;
+  const query = message.content;
+
+  console.log(`user: ${user} content: ${query}`);
+
+  const chatClient = new ChatClient(process.env.DIFY_API_KEY);
+
+  const response = await chatClient.createChatMessage(
+    inputs,
+    query,
+    user,
+    true,
+    null
+  );
+  const stream = response.data;
+
+  stream.on("data", (data) => {
+    console.log(data);
+  });
+
+  stream.on("end", () => {
+    console.log("stream done");
+  });
+  
+  return;
+});
+
+// Log in to Discord with your client's token
+client.login(process.env.DISCORD_TOKEN);
