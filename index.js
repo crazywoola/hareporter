4021b12a0c2482a418ae2eae328d1ac10c8fa954
crazywoola <427733928@qq.com>
2023-07-13 15:11:30 +0800

    WIP: add pg

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import { Client, Events, GatewayIntentBits } from 'discord.js';
 
 dotenv.config();
 import { ChatClient } from 'dify-client';
-
+import { Sequelize } from "sequelize"
 // Create a new client instance
 const client = new Client({
   intents: [
@@ -15,74 +15,88 @@ const client = new Client({
 });
 const chatClient = new ChatClient(process.env.DIFY_API_KEY);
 
-const messageDispatcher = (message) => {
-  if (message.author.bot) return;
-  if (message.mentions.has(client.user)) {
-    message.reply('Hello!');
-    // handleMessageCreate(message);
-  } else {
-    return;
-  }
-};
-
-const handleMessageCreate = async (message) => {
-  // Basic query to send to Dify
-  const inputs = {};
-  const user = `${message.author.username}-${message.author.id}`;
-  const query = message.content;
-  console.log(`user: ${user} content: ${query}`);
-
-  const response = await chatClient.createChatMessage(
-    inputs,
-    query,
-    user,
-    true,
-    null
-  );
-  const stream = response.data;
-
-  let msg = '';
-  let messageRef = null;
-
-  stream.on('data', async (chunk) => {
-    const completeString = chunk.toString();
-    const parsed = JSON.parse(
-      completeString.slice(completeString.indexOf('{'))
-    );
-    const result = parsed;
-
-    msg += result.answer;
-
-    if (msg.length === 0) {
-      messageRef = messageRef || (await message.reply('...'));
-    }
-
-    if (messageRef !== null && msg.length % 4 === 0) {
-      try {
-        await messageRef.edit(msg);
-      } catch (error) {
-        console.error('Error editing message:', error);
-      }
-    }
-  });
-
-  stream.on('end', () => {
-    console.log(`stream ended with msg: ${msg}`);
-    if (messageRef !== null) {
-      try {
-        messageRef.edit(msg);
-      } catch (error) {
-        console.error('Error editing message:', error);
-      }
-    }
-  });
-};
-
-// When the client is ready, run this code (only once)
-client.once(Events.ClientReady, (c) => {
-  console.log(`Ready! Logged in as ${c.user.tag}`);
+const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: 'postgres'
 });
 
-client.on(Events.MessageCreate, messageDispatcher);
-// Log in to Discord with your client's token
-client.login(process.env.DISCORD_TOKEN);
+sequelize.authenticate()
+    .then(() => {
+        console.info("INFO - Database connected.")
+    })
+    .catch((err) => {
+        console.error("ERROR - Unable to connect to the database:", err)
+    })
+    
+// const messageDispatcher = (message) => {
+//   if (message.author.bot) return;
+//   if (message.mentions.has(client.user)) {
+//     message.reply('Hello!');
+//     // handleMessageCreate(message);
+//   } else {
+//     return;
+//   }
+// };
+
+// const handleMessageCreate = async (message) => {
+//   // Basic query to send to Dify
+//   const inputs = {};
+//   const user = `${message.author.username}-${message.author.id}`;
+//   const query = message.content;
+//   console.log(`user: ${user} content: ${query}`);
+
+//   const response = await chatClient.createChatMessage(
+//     inputs,
+//     query,
+//     user,
+//     true,
+//     null
+//   );
+//   const stream = response.data;
+
+//   let msg = '';
+//   let messageRef = null;
+
+//   stream.on('data', async (chunk) => {
+//     const completeString = chunk.toString();
+//     const parsed = JSON.parse(
+//       completeString.slice(completeString.indexOf('{'))
+//     );
+//     const result = parsed;
+
+//     msg += result.answer;
+
+//     if (msg.length === 0) {
+//       messageRef = messageRef || (await message.reply('...'));
+//     }
+
+//     if (messageRef !== null && msg.length % 4 === 0) {
+//       try {
+//         await messageRef.edit(msg);
+//       } catch (error) {
+//         console.error('Error editing message:', error);
+//       }
+//     }
+//   });
+
+//   stream.on('end', () => {
+//     console.log(`stream ended with msg: ${msg}`);
+//     if (messageRef !== null) {
+//       try {
+//         messageRef.edit(msg);
+//       } catch (error) {
+//         console.error('Error editing message:', error);
+//       }
+//     }
+//   });
+// };
+
+// // When the client is ready, run this code (only once)
+// client.once(Events.ClientReady, (c) => {
+//   console.log(`Ready! Logged in as ${c.user.tag}`);
+// });
+
+// client.on(Events.MessageCreate, messageDispatcher);
+// // Log in to Discord with your client's token
+// client.login(process.env.DISCORD_TOKEN);
